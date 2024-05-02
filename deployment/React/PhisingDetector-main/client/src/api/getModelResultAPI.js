586f34e55f821c9ed8c841712831ff6e0e4accdf
Slander-Playz <sayonkardpsm12a@gmail.com>
2024-05-02 22:00:22 +0530

    fixes

--- deployment/React/PhisingDetector-main/client/src/api/getModelResultAPI.js
+++ deployment/React/PhisingDetector-main/client/src/api/getModelResultAPI.js
@@ -14,7 +14,7 @@ const getModelResultAPI = async (url) => {
     console.log("InputJSON" + inputJson);
 
     const response = await axios.post(
-      "https://2108-35-229-103-38.ngrok-free.app/url_prediction", // The public URL provided
+      "https://4b2b-35-202-88-8.ngrok-free.app/url_prediction", // The public URL provided
       inputJson, // Sending JSON as data
       {
         headers: {
